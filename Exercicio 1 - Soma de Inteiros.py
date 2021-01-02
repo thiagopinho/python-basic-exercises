502422bba096dbe4c60ad0dd439a2dc86133dd33
Thiago Pinho <thiagopinho05@gmail.com>
2021-01-01 22:46:59 -0300

    Primeiros exercícios de python

---
+++ Exercicio 1 - Soma de Inteiros.py
@@ -0,0 +1,8 @@
+#Faça um programa que peça dois números inteiros e imprima a soma desses dois números
+
+n1 = input("Digite o primeiro número inteiro")
+n2 = input("Digite o segundo número inteiro")
+
+soma = int(n1) + int(n2)
+
+print ("O valor da soma é:", soma)
---
+++ Exercicio 2 - Conversao metros em milimetros.py
@@ -0,0 +1,7 @@
+#Escreva um programa que leia um valor em metros e o exiba convertido em milímetros
+
+valor_metros = input("Digite um valor em metros: ")
+
+milimetros = int(valor_metros) * 1000 #Fórmula
+
+print ("Você digitou", valor_metros, "metro(s). Logo, o valor convertido é de:", milimetros ,"milímetros")
